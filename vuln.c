3ca1cb7f13bd9489732a77eb47dd4e2eecf179cc
metam0ng <oojm1210@naver.com>
2023-02-04 04:03:33 +0900

    binary add

---
+++ vuln.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    char buf[20];
+    int idx;
+
+    read(0, buf, 100);  //bof
+    scanf("%s", buf);   //bof
+    printf("%s", buf);  //leak
+    printf(buf);        //fsb
+
+    scanf("%d", idx);
+    buf[idx] = 'A';     //oob;
+}
